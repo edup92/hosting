5c132c4447b8fa8a4d3e758e209762f1f959332a
edup92 <edup92mail@gmail.com>
2025-12-25 02:42:15 +0000

    up

--- main.tf
+++ main.tf
@@ -388,7 +388,7 @@ resource "uptimerobot_monitor" "uptimerobot_main" {
   url               = "https://${each.value.domain}"
   interval          = 900
   keyword_type      = "ALERT_NOT_EXISTS"
-  keyword_value     = trimspace(each.value.monitor_keyword)
+  keyword_value     = "test"
   assigned_alert_contacts = [
     {
       alert_contact_id = "6819028",
